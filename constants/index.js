97f8bd4ca695c6c2e678f2017a9db4ba1f948a73
PollyT249 <polinatretiakova249@gmail.com>
2025-08-03 03:04:24 +0300

    Implement Navigation Bar

---
+++ constants/index.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    id: "cocktails",
+    title: "Cocktails",
+  },
+  {
+    id: "about",
+    title: "About Us",
+  },
+  {
+    id: "art",
+    title: "The Art",
+  },
+  {
+    id: "menu",
+    title: "Menu",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export { navLinks };
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from "./components/Navbar.jsx";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-indigo-300">Hello gsap</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
